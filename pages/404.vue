28e186498924cac89b183cd3e5bc6d208d2636ba
line_academy <115192748+LineAcademy@users.noreply.github.com>
2023-07-03 20:29:07 +0300

    Create 404.vue

---
+++ pages/404.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+const errors = useError()
+useHead({
+  title: 'Error',
+  meta: [
+      { name: 'description', content: 'My amazing site.' },
+      { name: 'robots', content: 'noindex, follow' },
+  ],
+})
+</script>
+
+
+<template>
+  <div>
+    <GlobalHeader />
+        <div class="banner__area p-relative z-index-3 banner__space blue-bg pt--50 pb--60" style="background-color: #F3F5F4;">
+            <div class="banner__text">
+            </div>
+            <div class="container">
+            <div class="row align-items-center">
+            <div class="col-12">
+            <div class="banner__section text-center mb--30">
+            <h2 class="text-primary"><span>.</span>  404 </h2>
+            <h2><span>.</span>الصفحة غير موجودة | Page Not Found</h2>
+            <NuxtLink class="main-btn" to="/"><span>Home Page | الرئيسية</span></NuxtLink>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            <GlobalFooter />
+  </div>
+</template>
