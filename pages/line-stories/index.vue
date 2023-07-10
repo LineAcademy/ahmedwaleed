c50a3d62a8db836718394cefbb898ff93360161a
line_academy <115192748+LineAcademy@users.noreply.github.com>
2023-07-10 15:36:31 +0300

    Update index.vue

--- pages/line-stories/index.vue
+++ pages/line-stories/index.vue
@@ -15,7 +15,6 @@
                                 src="9_zoz2x2.webp"
                                 alt="بانر صفحة حكايات برمجة مؤثرة لناس اشتغلت في البرمجة في شركات كبيرة ومتوسطة وكمان حكايات غير كدا كتير مع احمد برمجة المواقع"
                             />
-                            <!-- <img src="/assets/images/كورس بناء موقع جونيور.png" alt=""> -->
                         </div>
                     </div>
                     <!-- End Dashboard Top  -->
@@ -70,12 +69,12 @@
         }
       </style>
       <a href='https://www.youtube.com/embed/${p.video}?autoplay=1'>
-        <img src='https://img.youtube.com/vi/${p.video}/hqdefault.jpg' alt='Coffee Recipe Javascript Project'>
+        <img src='https://img.youtube.com/vi/${p.video}/hqdefault.jpg' alt='${p.description}'>
         <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
       </a>
       `"
       :src="`https://www.youtube.com/embed/${p.video}`" 
-      :title="`${p.title}`"
+      :title="`${p.description}`"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
       allowfullscreen>
