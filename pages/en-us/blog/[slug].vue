019f16ceca18af73b11a8cc9c31a8fd9827ee203
line_academy <115192748+LineAcademy@users.noreply.github.com>
2023-07-13 21:46:00 +0300

    Update [slug].vue

--- pages/en-us/blog/[slug].vue
+++ pages/en-us/blog/[slug].vue
@@ -5,47 +5,6 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="row g-5">
-                        <div class="col-lg-3">
-                            <!-- Start Dashboard Sidebar  -->
-                            <div class="rbt-default-sidebar sticky-top rbt-shadow-box rbt-gradient-border">
-                                <div class="inner">
-                                    <div class="content-item-content">
-
-                                        <div class="rbt-default-sidebar-wrapper">
-                                            <div class="section-title mb--20">
-                                                <h6 class="rbt-title-style-2">منشورات ذات صلة</h6>
-                                            </div>
-                                            <nav class="mainmenu-nav">
-                                                <ul class="dashboard-mainmenu rbt-default-sidebar-list">
-                                                    <li><a href="student-dashboard.html"><span>احمد وليد - Front-End</span></a></li>
-                                                    <li><a href="student-profile.html"><i class="feather-user"></i><span>My Profile</span></a></li>
-                                                    <li><a href="student-enrolled-articles.html"><i class="feather-book-open"></i><span>Enrolled articles</span></a></li>
-                                                    <li><a href="student-wishlist.html"><i class="feather-bookmark"></i><span>Wishlist</span></a></li>
-                                                    <li><a href="student-reviews.html"><i class="feather-star"></i><span>Reviews</span></a></li>
-                                                    <li><a href="student-my-quiz-attempts.html"><i class="feather-help-circle"></i><span>My Quiz Attempts</span></a></li>
-                                                    <li><a href="student-order-history.html"><i class="feather-shopping-bag"></i><span>Order History</span></a></li>
-                                                </ul>
-                                            </nav>
-
-                                            <div class="section-title mt--40 mb--20">
-                                                <h6 class="rbt-title-style-2">حكايات مؤثرة في عالم البرمجة</h6>
-                                            </div>
-
-                                            <nav class="mainmenu-nav">
-                                                <ul class="dashboard-mainmenu rbt-default-sidebar-list">
-                                                    <li><a href="student-settings.html"><span>حكاية سلمي لوظيفة React.JS</span></a></li>
-                                                    <li><a href="index.html"><span>حكاية مها بطل ابطال Front-End</span></a></li>
-                                                    <li><a href="index.html"><span>حكاية منة لوظيفة رياكت Canada</span></a></li>
-                                                </ul>
-                                            </nav>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- End Dashboard Sidebar  -->
-                        </div>
-
                         <div dir="rtl" class="col-lg-9">
                             <!-- Start Enrole Course  -->
                             <div class="rbt-dashboard-content pt-4">
@@ -97,6 +56,47 @@
                                 </div>
                             </div>
                             <!-- End Enrole Course  -->
+                        </div>
+
+			                            <div class="col-lg-3">
+                            <!-- Start Dashboard Sidebar  -->
+                            <div class="rbt-default-sidebar sticky-top rbt-shadow-box rbt-gradient-border">
+                                <div class="inner">
+                                    <div class="content-item-content">
+
+                                        <div class="rbt-default-sidebar-wrapper">
+                                            <div class="section-title mb--20">
+                                                <h6 class="rbt-title-style-2">منشورات ذات صلة</h6>
+                                            </div>
+                                            <nav class="mainmenu-nav">
+                                                <ul class="dashboard-mainmenu rbt-default-sidebar-list">
+                                                    <li><a href="student-dashboard.html"><span>احمد وليد - Front-End</span></a></li>
+                                                    <li><a href="student-profile.html"><i class="feather-user"></i><span>My Profile</span></a></li>
+                                                    <li><a href="student-enrolled-articles.html"><i class="feather-book-open"></i><span>Enrolled articles</span></a></li>
+                                                    <li><a href="student-wishlist.html"><i class="feather-bookmark"></i><span>Wishlist</span></a></li>
+                                                    <li><a href="student-reviews.html"><i class="feather-star"></i><span>Reviews</span></a></li>
+                                                    <li><a href="student-my-quiz-attempts.html"><i class="feather-help-circle"></i><span>My Quiz Attempts</span></a></li>
+                                                    <li><a href="student-order-history.html"><i class="feather-shopping-bag"></i><span>Order History</span></a></li>
+                                                </ul>
+                                            </nav>
+
+                                            <div class="section-title mt--40 mb--20">
+                                                <h6 class="rbt-title-style-2">حكايات مؤثرة في عالم البرمجة</h6>
+                                            </div>
+
+                                            <nav class="mainmenu-nav">
+                                                <ul class="dashboard-mainmenu rbt-default-sidebar-list">
+                                                    <li><a href="student-settings.html"><span>حكاية سلمي لوظيفة React.JS</span></a></li>
+                                                    <li><a href="index.html"><span>حكاية مها بطل ابطال Front-End</span></a></li>
+                                                    <li><a href="index.html"><span>حكاية منة لوظيفة رياكت Canada</span></a></li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Dashboard Sidebar  -->
                         </div>
                     </div>
 
