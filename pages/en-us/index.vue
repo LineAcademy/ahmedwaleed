f427572bcf101a76e7c68f4dd9e31905216411b4
line_academy <115192748+LineAcademy@users.noreply.github.com>
2023-07-14 01:13:53 +0300

    Update index.vue

--- pages/en-us/index.vue
+++ pages/en-us/index.vue
@@ -1310,11 +1310,6 @@ const { data: tes1 } = await useAsyncData("tes1", () => {
             { name: 'msapplication-tooltip', content: 'home page of Ahmed Waleed, a web developer trainer in Egypt' },
         ],
         link: [{ rel: 'stylesheet', href: '/assets/css/en.css' }],
-        htmlAttrs: { lang: 'en-US' },
-        script: [
-            { src: '/assets/js/vendor/modernizr.min.js', body: true },
-            { src: '/assets/js/vendor/jquery.js', body: true },
-            { src: '/assets/js/ahmedwaleed.min.js', body: true },
-          ],
+        htmlAttrs: { lang: 'en-US' }
 })  
   </script>
