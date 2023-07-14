c2f217bbeb1f582088f8e5ee754ba256488bc4ed
line_academy <115192748+LineAcademy@users.noreply.github.com>
2023-07-14 14:08:50 +0300

    Update [slug].vue

--- pages/en-us/hall-of-fame/[slug].vue
+++ pages/en-us/hall-of-fame/[slug].vue
@@ -172,7 +172,7 @@
 ])
 
     useHead({
-        titleTemplate: `${hall.name + hall.value.title} · hall of fame`,
+        titleTemplate: `${hall.name} ${hall.value.title} · hall of fame`,
         meta: [
             { name: 'description', content: `${hall.value.description}` },
             { name: 'keywords', content: 'People opinions on Programming trainer, students of Ahmed Waleed, web programmer, web programming, frontend, backend, javascript, react, view, next, typescript, web programming teacher, best web programmer in Egypt' },
